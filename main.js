b60f525774c6f75bac2862484f27de78045b488c
Mohit-Jay-Patel <113121401+Mohit-Jay-Patel@users.noreply.github.com>
2024-12-18 17:33:54 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+var sen ="Hi! This is my first hw."; //string
+var number = 220; //number
+var bol = true; //boolean
+var a1 = null;  //object
+var a2;  //undefined
+
+console.log(sen);
+console.log(number);
+console.log(bol);
+console.log(a1);
+console.log(a2);
+
+console.log(typeof sen);
+console.log(typeof number);
+console.log(typeof bol);
+console.log(typeof a1);
+console.log(typeof a2);
+
+var num2 = 22;
+
+console.log(number + num2);
+console.log(number - num2);
+console.log(number * num2);
+console.log(number / num2);
+console.log(number % num2);
+
